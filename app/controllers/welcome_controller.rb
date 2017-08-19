5bd0e10d33934798612e4809e36df254f7db91a2
n-kimberly <kimberlyvnguyen@gmail.com>
2017-08-19 00:22:53 -0700

    Generate Welcome Controller

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,11 @@
+# WelcomeController is a ruby class.
+class WelcomeController < ApplicationController
+
+  # Two empty methods that correspond to view names.
+  # When purpose is to invoke a view, method must be named with respect to the view.
+  def index
+  end
+
+  def about
+  end
+end
