8108f3170d6cbbcb932af8999452c42603efe9d6
n-kimberly <kimberlyvnguyen@gmail.com>
2017-09-02 12:11:00 -0700

    Validate Posts and Refactor View

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :topic
   has_many :comments, dependent: :destroy
+
+  validates :topic, presence: true
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+
 end
