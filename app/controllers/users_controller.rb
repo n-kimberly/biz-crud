b4b45e5eac2c76317d760e2180dbcff2673eb401
n-kimberly <kimberlyvnguyen@gmail.com>
2017-09-03 14:51:36 -0700

    Provide Confirmation Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,21 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    if @user.save
+      flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+      redirect_to root_path
+    else
+      flash.now[:alert] = "There was an error creating your account. Please try again."
+      render :new
+    end
+  end
+
   def create
     @user = User.new
     @user.name = params[:user][:name]
