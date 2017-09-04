fbd3bcde1e8155e659ffdf6cc886ca8ee3b8a1ff
n-kimberly <kimberlyvnguyen@gmail.com>
2017-09-04 15:16:27 -0700

    Add Scopes to Sort by Post Title and Create Date

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { order('title ASC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
 
   validates :topic, presence: true
   validates :user, presence: true
